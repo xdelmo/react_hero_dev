d49e93f4ee1bdfb1837f4eec0b5fcc82242d1901
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-28 18:54:51 +0100

    UPDATE: new svg comps + minor fixes

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import ImageHero from "./ImageHero";
+import AltaGioielleria from "./svg/AltaGioielleria";
+import Gioielleria from "./svg/Gioielleria";
+import Orologi from "./svg/Orologi";
 
 function HeroSection() {
   return (
     <div className="grid-main m-height-full">
-      <div className="grid-span-1-2 flex-center">
+      <div className="grid-span-1-2 flex-center hero-grid_section">
         <ImageHero />
+        <div>
+          <Gioielleria />
+        </div>
       </div>
-      <div className="grid-span-2-3 flex-center border-lateral">
+      <div className="grid-span-2-3 flex-center border-lateral hero-grid_section">
         <ImageHero />
+        <div>
+          <Orologi />
+        </div>
       </div>
-      <div className="grid-span-3-4 flex-center">
+      <div className="grid-span-3-4 flex-center hero-grid_section">
         <ImageHero />
+        <div>
+          <AltaGioielleria />
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/svg/AltaGioielleria.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+function AltaGioielleria() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="60.403"
+      //   height="60.518"
+      viewBox="0 0 60.403 60.518"
+    >
+      <g
+        id="Raggruppa_11"
+        data-name="Raggruppa 11"
+        transform="translate(-1338.502 -813.586)"
+      >
+        <g
+          id="Raggruppa_12"
+          data-name="Raggruppa 12"
+          transform="translate(1338.502 813.586)"
+        >
+          <path
+            id="Tracciato_73"
+            data-name="Tracciato 73"
+            d="M1085.559,1964.975l-2.056-7.692-7.759,11.671Z"
+            transform="translate(-1025.156 -1938.695)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_74"
+            data-name="Tracciato 74"
+            d="M1083.5,1998.194l2.056-7.7-9.815-3.975Z"
+            transform="translate(-1025.156 -1956.264)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_75"
+            data-name="Tracciato 75"
+            d="M1087.739,1946.87l-3.972-6.917-.974,5.177Z"
+            transform="translate(-1029.392 -1928.282)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_76"
+            data-name="Tracciato 76"
+            d="M1083.768,2022.68l3.972-6.917-4.945,1.736Z"
+            transform="translate(-1029.392 -1973.833)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_77"
+            data-name="Tracciato 77"
+            d="M1058.562,1925.833l-8.358,6.534,13.982-.9Z"
+            transform="translate(-1009.81 -1919.798)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_78"
+            data-name="Tracciato 78"
+            d="M1058.562,2037.367l5.624-5.632-13.982-.9Z"
+            transform="translate(-1009.81 -1982.888)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_79"
+            data-name="Tracciato 79"
+            d="M1012.907,1959.537l-11.272,1.357,6.813-11.823,4.458,10.466.036-.012-2.806-13.379,2.806-13.379-.036-.012-4.458,10.466-6.809-11.819,11.268,1.353.008-.032-12.972-4.267-10.17-9.132-.024.02,6.805,9.112H982.943l6.805-9.112-.024-.02-10.17,9.132-12.972,4.267.008.032,11.268-1.353-6.809,11.819-4.458-10.466-.032.012,2.806,13.379-2.81,13.379.036.012,4.458-10.466,6.813,11.823-11.272-1.357,0,.028,12.968,4.267,10.17,9.136.024-.02-6.809-9.117h13.619l-6.809,9.117.024.02,10.17-9.136,12.968-4.267Zm-23.158-9.456-8.717,11.2,5.325-13.172-14.058-1.96,14.058-1.96-5.325-13.168,8.717,11.188,8.717-11.188-5.325,13.168,14.062,1.96-14.062,1.96,5.325,13.172Z"
+            transform="translate(-959.549 -1915.886)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_80"
+            data-name="Tracciato 80"
+            d="M1042.293,1912.769l-7.68-2.056,6.223,12.569Z"
+            transform="translate(-1000.443 -1910.713)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_81"
+            data-name="Tracciato 81"
+            d="M1042.293,2041.347l-1.457-10.514-6.223,12.573Z"
+            transform="translate(-1000.443 -1982.888)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_82"
+            data-name="Tracciato 82"
+            d="M1022.663,1910.713h-7.939l3.968,3.417Z"
+            transform="translate(-988.492 -1910.713)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_83"
+            data-name="Tracciato 83"
+            d="M1014.724,2057.19h7.939l-3.972-3.417Z"
+            transform="translate(-988.492 -1996.672)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_84"
+            data-name="Tracciato 84"
+            d="M995.484,1912.769l1.453,10.513,6.227-12.569Z"
+            transform="translate(-976.932 -1910.713)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_85"
+            data-name="Tracciato 85"
+            d="M995.484,2041.347l7.68,2.06-6.227-12.573Z"
+            transform="translate(-976.932 -1982.888)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_86"
+            data-name="Tracciato 86"
+            d="M969.718,1925.833l-5.624,5.636,13.982.9Z"
+            transform="translate(-958.071 -1919.798)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_87"
+            data-name="Tracciato 87"
+            d="M969.718,2037.367l8.358-6.534-13.982.9Z"
+            transform="translate(-958.071 -1982.888)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_88"
+            data-name="Tracciato 88"
+            d="M949,1964.975l9.815,3.979-7.759-11.671Z"
+            transform="translate(-949.004 -1938.695)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_89"
+            data-name="Tracciato 89"
+            d="M949,1990.5l2.056,7.7,7.759-11.671Z"
+            transform="translate(-949.004 -1956.264)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_90"
+            data-name="Tracciato 90"
+            d="M958.121,1939.953l-3.968,6.917,4.945-1.74Z"
+            transform="translate(-952.098 -1928.282)"
+            fill="#242d1b"
+          />
+          <path
+            id="Tracciato_91"
+            data-name="Tracciato 91"
+            d="M954.154,2015.763l3.968,6.917.978-5.181Z"
+            transform="translate(-952.098 -1973.833)"
+            fill="#242d1b"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default AltaGioielleria;
